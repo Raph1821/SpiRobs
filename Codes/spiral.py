acb73611eee69f035141c6e2370842af9aae1153
Rylaks <sagnesjulien77@gmail.com>
2025-07-02 17:01:37 +0200

    update LaTeX

--- Codes/spiral.py
+++ Codes/spiral.py
@@ -8,7 +8,7 @@ b_values = [0.2, 0.5, 0.8]
 
 fig, axes = plt.subplots(len(b_values), len(a_values), figsize=(9, 6), subplot_kw={'aspect': 'equal'})
 
-theta = np.linspace(0, 2 * np.pi, 500)
+theta = np.linspace(0, 3 * np.pi, 500)
 
 for i, b in enumerate(b_values):  # lignes = b
     max_rho_line = max([np.max(a * np.exp(b * theta)) for a in a_values])
@@ -18,7 +18,7 @@ for i, b in enumerate(b_values):  # lignes = b
         y = rho * np.sin(theta)
 
         ax = axes[i, j]
-        ax.plot(x, y, label=f"a={a}, b={b}")
+        ax.plot(x, y, label=fr"$a_c$={a}, $b_c$={b}")
 
         # Afficher axes x=0 et y=0
         ax.axhline(0, color='gray', linestyle='--', linewidth=0.8)
@@ -27,15 +27,14 @@ for i, b in enumerate(b_values):  # lignes = b
         # Afficher les valeurs x et y (ticks)
         ax.set_xlim(-max_rho_line, max_rho_line)
         ax.set_ylim(-max_rho_line, max_rho_line)
-        ax.set_title(f"a={a}, b={b}", fontsize=10)
-        ax.legend(loc="upper left", fontsize=8)
+        ax.set_title(fr"$a_c$={a}, $b_c$={b}", fontsize=10)
 
         # Rendre les ticks plus lisibles
         ax.tick_params(axis='both', labelsize=8)
 
 # Ajouter flèches directionnelles
-fig.text(0.5, 0.98, "a augmente", ha='center', va='center', fontsize=14, weight='bold')
-fig.text(0.095, 0.5, "b\naugmente", ha='center', va='center', fontsize=14, weight='bold', rotation=90)
+fig.text(0.5, 0.98, r"$a_c$ augmente", ha='center', va='center', fontsize=14, weight='bold')
+fig.text(0.095, 0.5, r"$b_c$ augmente", ha='center', va='center', fontsize=14, weight='bold', rotation=90)
 
 arrow_a = FancyArrowPatch((0.2, 0.96), (0.8, 0.96), transform=fig.transFigure,
                           arrowstyle='->', mutation_scale=20, lw=2)
